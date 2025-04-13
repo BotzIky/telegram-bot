c541b41ee31fa024ca64257bd5399a7b39fa1b02
Riski Yanda <135003351+BotzIky@users.noreply.github.com>
2025-04-13 12:43:03 +0700

    Add files via upload

---
+++ actions/settings/settings.js
@@ -0,0 +1,27 @@
+const { Keyboard, Message } = require('../../utils/button');
+const config = require('../../config');
+
+const messages = {
+    id: {
+        about: `ℹ️ Akan datang...`,
+    },
+    en: {
+        about: `ℹ️ Coming soon...`,
+    }
+};
+
+module.exports = {
+    admin: true,
+    pattern: 'settings',
+    handler: async (ctx) => {
+        const language = ctx.session?.language || 'en';
+
+        const keyboard = Keyboard.keyboard(
+            Keyboard.row(
+                Keyboard.createButton('⬅️ Back', 'mainmenu')
+            )
+        );
+
+        await Message.edit(ctx, messages[language].about, keyboard);
+    }
+};
